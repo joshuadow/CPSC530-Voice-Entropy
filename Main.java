48f8f99e4c3c6b5fc0818454d70ebad38dcdac36
Josh Dow <joshua.dow@ucalgary.ca>
2018-11-01 17:20:59 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,40 @@
+import com.musicg.fingerprint.FingerprintSimilarity;
+import com.musicg.wave.Wave;
+import javax.sound.midi.SysexMessage;
+import java.io.File;
+import java.io.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Path currentRelativePath = Paths.get("");
+        String s = currentRelativePath.toAbsolutePath().toString();
+        File file1 = new File(args[0]);
+        File file2 = new File(args[1]);
+        file1.setReadable(true);
+        file2.setReadable(true);
+        byte[] file1Content = new byte[0];
+        byte[] file2Content = new byte[0];
+        try{
+            file1Content = Files.readAllBytes(file1.toPath());
+            file2Content = Files.readAllBytes(file2.toPath());
+        }
+        catch (IOException e){
+            System.out.println("IO Error on Entropy Calculation");
+        }
+        System.out.println("Entropy of first file: " + Entropy.getShannonEntropy(file1Content));
+        System.out.println("Entropy of second file: " + Entropy.getShannonEntropy(file2Content));
+
+        Wave sound1 = new Wave(s+"/"+file1);
+        Wave sound2 = new Wave(s+"/"+file2);
+        FingerprintSimilarity sim = sound1.getFingerprintSimilarity(sound2);
+        System.out.println("The files have a similarity score of: " + sim.getScore() + " matches per frame." +
+                " 1 is average, anything >= 1.5 is most likely the same");
+
+    }
+}
